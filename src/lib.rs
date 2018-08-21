5892f5ddffb5cddac9b968e1b5f4aed4451b2022
jdvr <juandavidvegarguez@gmail.com>
2018-08-21 23:47:43 +0200

    make all inmutable using iterator

--- src/lib.rs
+++ src/lib.rs
@@ -17,39 +17,19 @@ pub fn find_saddle_points(input: &[Vec<u64>]) -> Vec<(usize, usize)> {
 }
 
 fn row_maxs(input: &[Vec<u64>]) -> Vec<Point> {
-    let mut points_by_rows: Vec<Point> = Vec::new();
-    for row_number in 0..input.len(){
-        let mut row_max = input[row_number][0];
-        let mut points_by_row: Vec<Point> = Vec::new();
-        for col_number in 0..input[row_number].len() {
-            if row_max <= input[row_number][col_number] {
-                row_max = input[row_number][col_number];
-                points_by_row.drain_filter(|p| input[p.x][p.y] < row_max);
-                points_by_row.push(Point { x: row_number, y: col_number});
-            }
-        }
-        println!("{:?}", points_by_row);
-        points_by_rows.append(&mut points_by_row);
-    }
-    points_by_rows
+    (0..input.len()).map(|row_number| {
+        let row_max = input[row_number].iter().max().unwrap();
+        input[row_number].iter().enumerate().filter(|(_, val)| val == &row_max).map(|(i, _)| Point {x: row_number, y: i}).collect()
+    }).flat_map(|v: Vec<Point>| v).collect()
 }
 
 
 fn col_mins(input: &[Vec<u64>]) -> Vec<Point> {
-    let mut points_by_columns: Vec<Point> = Vec::new();
-    for col_number in 0..input[0].len(){
-        let mut col_min = u64::max_value();
-        let mut points_by_column: Vec<Point> = Vec::new();
-        for row_number in 0..input.len() {
-            if col_min >= input[row_number][col_number] {
-                col_min = input[row_number][col_number];
-                points_by_column.drain_filter(|p| input[p.x][p.y] > col_min);
-                points_by_column.push(Point { x: row_number, y: col_number});
-            }
-        }
-        points_by_columns.append(&mut points_by_column);
-    }
-    points_by_columns
+    (0..input[0].len()).map(|col_number| {
+        let col_min = (0..input.len()).map(|i| input[i][col_number]).min().unwrap();
+        (0..input.len()).map(|i| input[i][col_number]).enumerate().filter(|(_, val)| val == &col_min).map(|(i, _)| Point {x: i, y: col_number}).collect()
+    }).flat_map(|v: Vec<Point>| v).collect()
+    
 }
 
 fn saddle_points(row_points: Vec<Point>, column_points: Vec<Point>) -> Vec<(usize, usize)> {
